36245533570ce9860e13ad34a9a40663756cedfe
Awrish <awrishkhan@gmail.com>
2024-12-01 12:23:58 -0500

    Restructed files

---
+++ 2023/Day01/main.py
@@ -0,0 +1,14 @@
+def part1(self, fileName):
+    f = open(filename, "r")
+    Lines = f.readlines()
+
+    for line in Lines:
+        print(line[0])
+    
+
+
+
+
+
+inputPath = r'C:\Users\Awrish\Documents\Projects\AdventofCode\Solutions\2023\Day01\input.txt'
+self.part1(inputPath)
